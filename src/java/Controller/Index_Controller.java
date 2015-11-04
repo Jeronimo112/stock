0eb191ebb9f5e9839d24cab487d27ce23689f18e
john <john@john-HP-Pavilion-17-Notebook-PC>
2015-11-04 17:08:46 +0100

    Navigation läuftj

--- src/java/Controller/Index_Controller.java
+++ src/java/Controller/Index_Controller.java
@@ -44,7 +44,6 @@ public class Index_Controller extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/charts.jsp");
 			rd.forward(request, response);
                         Stock stock = YahooFinance.get("sdf");
-                        
                         stock.print();
                         stock.print();
                         stock.print();
